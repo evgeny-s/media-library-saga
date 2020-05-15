0b258545308daf766dae44474d97b47b7176a2c9
Evgeny Svirsky <evgeny@cere.io>
2020-05-15 20:04:24 +0300

    * Added Material UI

--- src/components/CategoryList.js
+++ src/components/CategoryList.js
@@ -1,5 +1,19 @@
 import React, {useEffect} from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import Table from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/TableBody';
+import TableCell from '@material-ui/core/TableCell';
+import TableContainer from '@material-ui/core/TableContainer';
+import TableHead from '@material-ui/core/TableHead';
+import TableRow from '@material-ui/core/TableRow';
+import Paper from '@material-ui/core/Paper';
 import PropTypes from 'prop-types';
+
+const useStyles = makeStyles({
+  table: {
+    minWidth: 650,
+  },
+});
 
 function CategoryList({fetchCategories, items, fetchCategoryError}) {
   useEffect(() => {
@@ -12,17 +26,32 @@ function CategoryList({fetchCategories, items, fetchCategoryError}) {
     }
   }, [fetchCategoryError]);
 
+  const classes = useStyles();
+
   return (
     <div>
-      {
-        items.map(({id, title, description}) => (
-          <div key={id}>
-            <span>{id}</span>
-            <span>{title}</span>
-            <span>{description}</span>
-          </div>
-        ))
-      }
+      <TableContainer component={Paper}>
+        <Table className={classes.table} aria-label="simple table">
+          <TableHead>
+            <TableRow>
+              <TableCell>#</TableCell>
+              <TableCell align="right">Title</TableCell>
+              <TableCell align="right">Description</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {items.map(({id, title, description}) => (
+              <TableRow key={id}>
+                <TableCell component="th" scope="row">
+                  {id}
+                </TableCell>
+                <TableCell align="right">{title}</TableCell>
+                <TableCell align="right">{description}</TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </TableContainer>
     </div>
   );
 }
